3c5c47eabf09e18792377dfd0f36720591ddf94a
David Romero <drorganvidez@us.es>
2022-02-15 13:56:44 +0100

    feat: Adds dashboard routes

---
+++ api/controllers/dashboardController.js
@@ -0,0 +1,57 @@
+'use strict'
+
+const mongoose = require('mongoose')
+
+/* ---------------DASHBOARD---------------------- */
+
+exports.get_trip_average = function (req, res) {
+    res.status(200)
+}
+
+exports.get_trip_minimum = function (req, res) {
+    res.status(200)
+}
+
+exports.get_trip_maximum = function (req, res) {
+    res.status(200)
+}
+
+exports.get_trip_deviation = function (req, res) {
+    res.status(200)
+}
+
+exports.get_application_average = function (req, res) {
+    res.status(200)
+}
+
+exports.get_application_minimum = function (req, res) {
+    res.status(200)
+}
+
+exports.get_application_maximum = function (req, res) {
+    res.status(200)
+}
+
+exports.get_application_deviation = function (req, res) {
+    res.status(200)
+}
+
+exports.get_trip_price_average = function (req, res) {
+    res.status(200)
+}
+
+exports.get_trip_price_minimum = function (req, res) {
+    res.status(200)
+}
+
+exports.get_trip_price_maximum = function (req, res) {
+    res.status(200)
+}
+
+exports.get_trip_price_deviation = function (req, res) {
+    res.status(200)
+}
+
+exports.get_ratio_by_status = function (req, res) {
+    res.status(200)
+}
