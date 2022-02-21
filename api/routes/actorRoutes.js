1cba4bd5d8ad649dcbaef256cb97eb12602375b7
David Romero <davromorg@alum.us.es>
2022-02-21 13:50:04 +0100

    feat: Arregla ruta errónea en actors

--- api/routes/actorRoutes.js
+++ api/routes/actorRoutes.js
@@ -9,7 +9,9 @@ module.exports = function (app) {
   app.route('/actors/:actorId')
     .get(actors.read_an_actor)
     .put(actors.update_an_actor)
-    //TODO: .put(actors.ban_an_actor)
     .delete(actors.delete_an_actor)
+
+    app.route('/actors/:actorId/ban')
+      .put(actors.ban_an_actor)
 
 }
