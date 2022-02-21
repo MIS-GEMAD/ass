f13c37dfb18369671890b4e436330afbb62a0b37
David Romero <davromorg@alum.us.es>
2022-02-21 14:25:17 +0100

    feat: Validate actor model

--- api/models/actorModel.js
+++ api/models/actorModel.js
@@ -1,6 +1,7 @@
 "use strict";
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt')
 
 const validateEmail = function (email) {
   const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -9,28 +10,25 @@ const validateEmail = function (email) {
 
 const ActorSchema = new Schema(
   {
-    role: {
+    role: [{
       type: String,
       enum : ['ADMINISTRATOR','MANAGER', 'EXPLORER', 'SPONSOR'],
       default: 'EXPLORER',
-      required: [true, "Role is required"],
-    },
+      required: 'Kindly enter the user role(s)'
+    }],
     name: {
       type: String,
-      required: [true, "Name is required"],
+      required: 'Kindly enter the actor name'
     },
     surname: {
       type: String,
-      required: [true, "Surname is required"],
+      required: 'Kindly enter the actor surname'
     },
     email: {
       type: String,
-      validate: [validateEmail, "Please provide a valid email"],
-      match: [
-        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-        "Please provide a valid email",
-      ],
-      required: [true, "Email is required"],
+      required: 'Kindly enter the actor email',
+      unique: true,
+      match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
     },
     phone: {
       type: String,
@@ -57,8 +55,42 @@ const ActorSchema = new Schema(
       type: [Schema.Types.ObjectId],
       ref: 'Sponsorship',
     }],
+    validated: {
+      type: Boolean,
+      default: false
+    },
+    created: {
+      type: Date,
+      default: Date.now
+    }
   },
   { strict: false }
 );
+
+ActorSchema.pre('save', function (callback) {
+  const actor = this
+  // Break out if the password hasn't changed
+  if (!actor.isModified('password')) return callback()
+
+  // Password changed so we need to hash it
+  bcrypt.genSalt(5, function (err, salt) {
+    if (err) return callback(err)
+
+    bcrypt.hash(actor.password, salt, function (err, hash) {
+      if (err) return callback(err)
+      actor.password = hash
+      callback()
+    })
+  })
+})
+
+ActorSchema.methods.verifyPassword = function (password, cb) {
+  bcrypt.compare(password, this.password, function (err, isMatch) {
+    // console.log('verifying password in actorModel: ' + password)
+    if (err) return cb(err)
+    // console.log('iMatch: ' + isMatch)
+    cb(null, isMatch)
+  })
+}
 
 module.exports = mongoose.model("Actor", ActorSchema);
