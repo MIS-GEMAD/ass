e3758cc19f803aa7fe49cef4ec09ace7646d25b0
GermanMT <germanoctako@gmail.com>
2022-03-02 12:26:16 +0100

    feat: new dashboard model

---
+++ api/models/dashboardMode.js
@@ -0,0 +1,62 @@
+"use strict";
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const DashboardSchema = new Schema(
+  {
+    trip_average: {
+      type: Number
+    },
+    trip_minimum: {
+      type: Number
+    },
+    trip_maximum : {
+      type: Number
+    },
+    trip_deviation: {
+      type: Number
+    },
+    application_average: {
+      type: Number
+    },
+    application_minimum: {
+      type: Number
+    },
+    application_maximum: {
+      type: Number
+    },
+    application_deviation: {
+      type: Number
+    },
+    trip_price_average: {
+      type: Number
+    },
+    trip_price_minimum: {
+      type: Number
+    },
+    trip_price_maximum: {
+      type: Number
+    },
+    trip_price_deviation: {
+      type: Number
+    },
+    ratio_by_status: {
+      type: Number
+    },
+    ratioCancelledOrders: {
+      type: Number,
+      max: 1,
+      min: 0
+    },
+    computationMoment: {
+      type: Date,
+      default: Date.now
+    },
+    rebuildPeriod: {
+      type: String
+    }
+  },
+  { strict: false }
+);
+
+module.exports = mongoose.model("Dashboard", DashboardSchema);
--- api/routes/dashboardRoutes.js
+++ api/routes/dashboardRoutes.js
@@ -1,45 +1,13 @@
 'use strict'
 module.exports = function (app) {
   
-    const dashboard = require('../controllers/dashboardController')
+  const dashboard = require('../controllers/dashboardController')
 
-    app.route('/dashboard/trips/average')
-      .get(dashboard.get_trip_average)
+  app.route('/dashboard')
+    .get(dashboard.list_all_indicators)
+    .post(dashboard.rebuildPeriod)
 
-    app.route('/dashboard/trips/minimum')
-      .get(dashboard.get_trip_minimum)
-
-    app.route('/dashboard/trips/maximum')
-      .get(dashboard.get_trip_maximum)
-
-    app.route('/dashboard/trips/deviation')
-      .get(dashboard.get_trip_deviation)
-
-    app.route('/dashboard/applications/average')
-      .get(dashboard.get_application_average)
-
-    app.route('/dashboard/applications/minimum')
-      .get(dashboard.get_application_minimum)
-
-    app.route('/dashboard/applications/maximum')
-      .get(dashboard.get_application_maximum)
-
-    app.route('/dashboard/applications/deviation')
-      .get(dashboard.get_application_deviation)
-
-    app.route('/dashboard/trips/price/average')
-      .get(dashboard.get_trip_price_average)
-
-    app.route('/dashboard/trips/price/minimum')
-      .get(dashboard.get_trip_price_minimum)
-
-    app.route('/dashboard/trips/price/maximum')
-      .get(dashboard.get_trip_price_maximum)
-
-    app.route('/dashboard/trips/price/deviation')
-      .get(dashboard.get_trip_price_deviation)
-
-    app.route('/dashboard/applications/status')
-      .get(dashboard.get_ratio_by_status)
+  app.route('/dashboard/latest')
+    .get(dashboard.last_indicator)
 
 }
