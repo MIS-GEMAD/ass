eb3489f17aa3f6d779c2c0279623af3b3dee8b35
emiliacoleto <emiliacoleto@gmaill.com>
2022-03-15 20:14:54 +0100

    sponsorship routes developed

--- api/controllers/sponsorshipController.js
+++ api/controllers/sponsorshipController.js
@@ -3,78 +3,156 @@
 const mongoose = require('mongoose')
 
 const Sponsorship = mongoose.model('Sponsorship')
+const Actor = mongoose.model('Actor')
 
 exports.list_all_sponsorships = function (req, res) {
-  Sponsorship.find({}, function (err, sponsorship) {
+  Actor.findById(req.params.actorId, function (err, sponsor) {
     if (err) {
-      res.status(400).send(err)
+      res.status(400).send(err);
     } else {
-      res.status(200).json(sponsorship)
+      if (!sponsor) {
+        res.status(404).send("Sponsor not found");
+      } else if (!sponsor.role.find((role) => role === "SPONSOR")) {
+        res.status(400).send("Actor must be sponsor");
+      } else {
+        Sponsorship.find({actor: req.params.actorId}, function (err, sponsorship) {
+          if (err) {
+            res.status(400).send(err)
+          } else {
+            res.status(200).json(sponsorship)
+          }
+        })
+      }
     }
   })
 }
 
 exports.create_a_sponsorship = function (req, res) {
-  const newSponsorship = new Sponsorship(req.body)
-
-  newSponsorship.save(function (error, sponsorship) {
-    if (error) {
-      res.status(400).send(error)
+  Actor.findById(req.params.actorId, function (err, sponsor) {
+    if (err) {
+      res.status(400).send(err);
     } else {
-      res.status(201).json(sponsorship)
+      if (!sponsor) {
+        res.status(404).send("Sponsor not found");
+      } else if (!sponsor.role.find((role) => role === "SPONSOR")) {
+        res.status(400).send("Actor must be sponsor");
+      } else {
+        const newSponsorship = new Sponsorship(req.body)
+        newSponsorship.save(function (error, sponsorship) {
+          if (error) {
+            res.status(400).send(error)
+          } else {
+            res.status(201).json(sponsorship)
+          }
+        })
+      }
     }
   })
 }
 
 exports.read_a_sponsorship = function (req, res) {
-  Sponsorship.findById(req.params.sponsorshipId, function (err, sponsorship) {
+  Actor.findById(req.params.actorId, function (err, sponsor) {
     if (err) {
-      res.status(400).send(err)
+      res.status(400).send(err);
     } else {
-      res.status(200).json(sponsorship)
+      if (!sponsor) {
+        res.status(404).send("Sponsor not found");
+      } else if (!sponsor.role.find((role) => role === "SPONSOR")) {
+        res.status(400).send("Actor must be sponsor");
+      } else {
+        Sponsorship.findById(req.params.sponsorshipId, function (err, sponsorship) {
+          if (err) {
+            res.status(400).send(err)
+          } else {
+            res.status(200).json(sponsorship)
+          }
+        })
+      }
     }
   })
 }
 
 exports.update_a_sponsorship = function (req, res) {
-  Sponsorship.findById(req.params.sponsorshipId, function (err, sponsorship) {
+  Actor.findById(req.params.actorId, function (err, sponsor) {
     if (err) {
-      res.status(404).send(err)
+      res.status(400).send(err);
     } else {
-      Sponsorship.findOneAndUpdate({ _id: req.params.sponsorshipId }, req.body, { new: true }, function (err, sponsorship) {
-        if (err) {
-          res.status(400).send(err)
-        } else {
-          res.status(201).json(sponsorship)
-        }
-      })
+      if (!sponsor) {
+        res.status(404).send("Sponsor not found");
+      } else if (!sponsor.role.find((role) => role === "SPONSOR")) {
+        res.status(400).send("Actor must be sponsor");
+      } else {
+        Sponsorship.findById(req.params.sponsorshipId, function (err, sponsorship) {
+          if (err) {
+            res.status(404).send(err)
+          } else {
+            Sponsorship.findOneAndUpdate({ _id: req.params.sponsorshipId }, req.body, { new: true }, function (err, sponsorship) {
+              if (err) {
+                res.status(400).send(err)
+              } else {
+                res.status(201).json(sponsorship)
+              }
+            })
+          }
+        })
+      }
     }
   })
 }
 
 exports.delete_a_sponsorship = function (req, res) {
-  Sponsorship.deleteOne({
-    _id: req.params.sponsorshipId
-  }, function (err, sponsorship) {
+  Actor.findById(req.params.actorId, function (err, sponsor) {
     if (err) {
-      res.status(404).send(err)
+      res.status(400).send(err);
     } else {
-      res.status(204).json({ message: 'Sponsorship successfully deleted' })
+      if (!sponsor) {
+        res.status(404).send("Sponsor not found");
+      } else if (!sponsor.role.find((role) => role === "SPONSOR")) {
+        res.status(400).send("Actor must be sponsor");
+      } else {
+        Sponsorship.deleteOne({
+          _id: req.params.sponsorshipId
+        }, function (err, sponsorship) {
+          if (err) {
+            res.status(404).send(err)
+          } else {
+            res.status(204).json({ message: 'Sponsorship successfully deleted' })
+          }
+        })
+      }
     }
   })
 }
 
 // TODO: Implementar via PayPal
 exports.pay_a_sponsorship = function (req, res) {
-  res.status(200)
-}
-
-exports.list_actor_sponsorships = function (req, res) {
-  Sponsorship.find({actor_id: req.params.actorId}, function (err, sponsorships) {
+  Actor.findById(req.params.actorId, function (err, sponsor) {
     if (err) {
-      res.status(400).send(err)
+      res.status(400).send(err);
     } else {
-      res.status(200).json(sponsorships)
+      if (!sponsor) {
+        res.status(404).send("Sponsor not found");
+      } else if (!sponsor.role.find((role) => role === "SPONSOR")) {
+        res.status(400).send("Actor must be sponsor");
+      } else {
+        Sponsorship.findById(req.params.sponsorshipId, function (err, sponsorship) {
+          if (err) {
+            res.status(404).send("Sponsor not found")
+          } else {
+            if(sponsorship.is_paid === true) {
+              res.status(400).send("Sponsorship is already payed");
+            } else {
+              Sponsorship.findOneAndUpdate({ _id: req.params.sponsorshipId }, {is_paid: true}, { new: true }, function (err, sponsorship) {
+                if (err) {
+                  res.status(400).send(err)
+                } else {
+                  res.status(201).json(sponsorship)
+                }
+              })
+            }
+          }
+        })
+      }
     }
   })
 }
