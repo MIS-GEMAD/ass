4659416967f6524a33396a5eb9b40d95838e38bd
David Romero <davromorg@alum.us.es>
2022-03-20 20:53:45 +0100

    fix: Fix Sponsorship CRUDs

--- api/controllers/sponsorshipController.js
+++ api/controllers/sponsorshipController.js
@@ -5,123 +5,169 @@ const mongoose = require('mongoose')
 const Sponsorship = mongoose.model('Sponsorship')
 const Actor = mongoose.model('Actor')
 
-exports.list_all_sponsorships = function (req, res) {
-  Actor.findById(req.params.actorId, function (err, sponsor) {
+const authController = require('../controllers/authController')
+
+exports.list_all_sponsorships = async function (req, res) {
+
+  // get auth sponsor
+  const idToken = req.header('idToken')
+  let authSponsorId = await authController.getUserId(idToken)
+  authSponsorId = String(authSponsorId)
+
+  Actor.findById(authSponsorId, function (err, sponsor) {
     if (err) {
       res.status(400).send(err);
-    } else {
-      if (!sponsor) {
-        res.status(404).send("Sponsor not found");
-      } else if (!sponsor.role.find((role) => role === "SPONSOR")) {
-        res.status(400).send("Actor must be sponsor");
-      } else {
-        Sponsorship.find({actor: req.params.actorId}, function (err, sponsorship) {
-          if (err) {
-            res.status(400).send(err)
-          } else {
-            res.status(200).json(sponsorship)
-          }
-        })
-      }
+    } else{
+
+      Sponsorship.find({actor: authSponsorId}, function (err, sponsorship) {
+        if (err) {
+          res.status(400).send(err)
+        } else {
+          res.status(200).json(sponsorship)
+        }
+      })
+
     }
-  })
+  });
+
 }
 
-exports.create_a_sponsorship = function (req, res) {
-  Actor.findById(req.params.actorId, function (err, sponsor) {
+exports.create_a_sponsorship = async function (req, res) {
+
+  // get auth sponsor
+  const idToken = req.header('idToken')
+  let authSponsorId = await authController.getUserId(idToken)
+  authSponsorId = String(authSponsorId)
+
+  Actor.findById(authSponsorId, function (err, sponsor) {
     if (err) {
       res.status(400).send(err);
-    } else {
-      if (!sponsor) {
-        res.status(404).send("Sponsor not found");
-      } else if (!sponsor.role.find((role) => role === "SPONSOR")) {
-        res.status(400).send("Actor must be sponsor");
-      } else {
-        const newSponsorship = new Sponsorship(req.body)
-        newSponsorship.save(function (error, sponsorship) {
-          if (error) {
-            res.status(400).send(error)
-          } else {
-            res.status(201).json(sponsorship)
-          }
-        })
-      }
+    } else{
+      req.body.actor = authSponsorId
+      const newSponsorship = new Sponsorship(req.body)
+      newSponsorship.save(function (error, sponsorship) {
+        if (error) {
+          res.status(400).send(error)
+        } else {
+
+          // updated the sponsor sponsorships list
+          Actor.findOneAndUpdate({ _id: authSponsorId }, {"$push": {sponsorships: sponsorship._id}}, { new: true }, function(err, result){
+            if(err){
+              res.send(err)
+            }
+            else{
+              res.status(201).json(sponsorship);
+            }
+          })
+
+        }
+      })
     }
-  })
+  });
+
 }
 
-exports.read_a_sponsorship = function (req, res) {
-  Actor.findById(req.params.actorId, function (err, sponsor) {
+exports.read_a_sponsorship = async function (req, res) {
+
+  // get auth sponsor
+  const idToken = req.header('idToken')
+  let authSponsorId = await authController.getUserId(idToken)
+  authSponsorId = String(authSponsorId)
+
+  Actor.findById(authSponsorId, function (err, sponsor) {
     if (err) {
       res.status(400).send(err);
-    } else {
-      if (!sponsor) {
-        res.status(404).send("Sponsor not found");
-      } else if (!sponsor.role.find((role) => role === "SPONSOR")) {
-        res.status(400).send("Actor must be sponsor");
-      } else {
-        Sponsorship.findById(req.params.sponsorshipId, function (err, sponsorship) {
-          if (err) {
-            res.status(400).send(err)
+    } else{
+      Sponsorship.findById(req.params.sponsorshipId, function (err, sponsorship) {
+        if (err) {
+          res.status(400).send(err)
+        } else {
+
+          if(sponsorship != null) {
+            // check is my sponsorship
+            if(sponsorship.actor == authSponsorId) {
+              res.status(200).json(sponsorship)
+            }else{
+              res.status(400).send("This sponsor does not have permission to view this sponsorship")
+            }
           } else {
-            res.status(200).json(sponsorship)
+            res.sendStatus(404)
           }
-        })
-      }
+
+
+        }
+      })
     }
-  })
+  });
+
 }
 
-exports.update_a_sponsorship = function (req, res) {
-  Actor.findById(req.params.actorId, function (err, sponsor) {
+exports.update_a_sponsorship = async function (req, res) {
+
+  // get auth sponsor
+  const idToken = req.header('idToken')
+  let authSponsorId = await authController.getUserId(idToken)
+  authSponsorId = String(authSponsorId)
+
+  Actor.findById(authSponsorId, function (err, sponsor) {
     if (err) {
       res.status(400).send(err);
     } else {
-      if (!sponsor) {
-        res.status(404).send("Sponsor not found");
-      } else if (!sponsor.role.find((role) => role === "SPONSOR")) {
-        res.status(400).send("Actor must be sponsor");
-      } else {
-        Sponsorship.findById(req.params.sponsorshipId, function (err, sponsorship) {
-          if (err) {
-            res.status(404).send(err)
-          } else {
-            Sponsorship.findOneAndUpdate({ _id: req.params.sponsorshipId }, req.body, { new: true }, function (err, sponsorship) {
-              if (err) {
-                res.status(400).send(err)
-              } else {
-                res.status(201).json(sponsorship)
-              }
-            })
-          }
-        })
-      }
+
+      Sponsorship.findById(req.params.sponsorshipId, function (err, sponsorship) {
+        if (err) {
+          res.status(404).send(err)
+        } else {
+          Sponsorship.findOneAndUpdate({ _id: req.params.sponsorshipId }, req.body, { new: true }, function (err, sponsorship) {
+            if (err) {
+              res.status(400).send(err)
+            } else {
+              res.status(201).json(sponsorship)
+            }
+          })
+        }
+      })
+
     }
-  })
+  });
+
 }
 
-exports.delete_a_sponsorship = function (req, res) {
-  Actor.findById(req.params.actorId, function (err, sponsor) {
+exports.delete_a_sponsorship = async function (req, res) {
+
+  // get auth sponsor
+  const idToken = req.header('idToken')
+  let authSponsorId = await authController.getUserId(idToken)
+  authSponsorId = String(authSponsorId)
+
+  Actor.findById(authSponsorId, function (err, sponsor) {
     if (err) {
       res.status(400).send(err);
     } else {
-      if (!sponsor) {
-        res.status(404).send("Sponsor not found");
-      } else if (!sponsor.role.find((role) => role === "SPONSOR")) {
-        res.status(400).send("Actor must be sponsor");
-      } else {
-        Sponsorship.deleteOne({
-          _id: req.params.sponsorshipId
-        }, function (err, sponsorship) {
-          if (err) {
-            res.status(404).send(err)
-          } else {
-            res.status(204).json({ message: 'Sponsorship successfully deleted' })
-          }
-        })
-      }
+
+      Sponsorship.deleteOne({
+        _id: req.params.sponsorshipId
+      }, function (err, sponsorship) {
+        if (err) {
+          res.status(404).send(err)
+        } else {
+
+          // updated the sponsor sponsorships list
+          Actor.findOneAndUpdate({ _id: authSponsorId }, {"$pull": {sponsorships: req.params.sponsorshipId}}, { new: true }, function(err, result){
+            if(err){
+              res.send(err)
+            }
+            else{
+              res.status(204).json({ message: 'Sponsorship successfully deleted' })
+            }
+          })
+          
+        }
+      })
+
     }
-  })
+  });
+
 }
 
 // TODO: Implementar via PayPal
--- api/routes/sponsorshipRoutes.js
+++ api/routes/sponsorshipRoutes.js
@@ -3,15 +3,17 @@ module.exports = function (app) {
 
   const sponsorship = require('../controllers/sponsorshipController')
 
-  app.route('/actors/:actorId/sponsorships')
-    .get(sponsorship.list_all_sponsorships)
-    .post(sponsorship.create_a_sponsorship)
+  const authController = require('../controllers/authController')
 
-  app.route('/actors/:actorId/sponsorships/:sponsorshipId')
-    .get(sponsorship.read_a_sponsorship)
-    .put(sponsorship.update_a_sponsorship)
-    .delete(sponsorship.delete_a_sponsorship)
+  app.route('/sponsorships')
+    .get(authController.verifyUser(['SPONSOR']), sponsorship.list_all_sponsorships)
+    .post(authController.verifyUser(['SPONSOR']), sponsorship.create_a_sponsorship)
 
-  app.route('/actors/:actorId/sponsorships/:sponsorshipId/pay')
-    .put(sponsorship.pay_a_sponsorship)
+  app.route('/sponsorships/:sponsorshipId')
+    .get(authController.verifyUser(['SPONSOR']), sponsorship.read_a_sponsorship)
+    .put(authController.verifyUser(['SPONSOR']), sponsorship.update_a_sponsorship)
+    .delete(authController.verifyUser(['SPONSOR']), sponsorship.delete_a_sponsorship)
+
+  app.route('/sponsorships/:sponsorshipId/pay')
+    .put(authController.verifyUser(['SPONSOR']), sponsorship.pay_a_sponsorship)
 }
